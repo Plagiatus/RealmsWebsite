69db139fecf8b00f7134d8015c4fb1319190dd3f
Lukas Scheuerle (Plagiatus) <lukas@spark-squared.com>
2019-12-18 09:20:07 +0100

    sort realms overview

--- server-npm/java/realms.js
+++ server-npm/java/realms.js
@@ -19,16 +19,22 @@ var overview;
             xhr.send(JSON.stringify(data));
             if (xhr.response) {
                 let result = JSON.parse(xhr.response);
-                console.log(result);
                 if (result.error) {
                     displayError(result.error);
                     return;
                 }
                 if (result.servers && result.servers.length > 0) {
+                    console.log(result.servers);
+                    result.servers = result.servers.sort(sortRealms);
                     realmsList.innerHTML = "";
+                    console.log(result.servers);
                     for (let s of result.servers) {
+                        // console.log(s);
                         createOneRealm(s, data.name);
                     }
+                }
+                else {
+                    realmsList.innerHTML = "Nothing to see here. You don't seem to have";
                 }
             }
         }
@@ -39,14 +45,27 @@ var overview;
     function createOneRealm(_server, ownerName) {
         let owner = _server.owner == ownerName;
         realmsList.innerHTML +=
-            `<div class="realm">
-  <img src="https://crafatar.com/avatars/${_server.ownerUUID}?size=40&overlay" alt="">
-  <span>${_server.properties.name || ""}</span>
-  <span>${_server.minigameName ? "Minigame: " + _server.minigameName : _server.properties.description || ""}</span>
-  <span>${_server.owner}</span>
-  <button onclick="${owner ? "overview.selectRealm(" + _server.id + ")" : "overview.leaveRealm(" + _server.id + ")"}">${owner ? "Edit" : "Leave"}</button>
-  </div>`;
+            `<div class="realm ${owner ? "owned" : "notOwned"}">
+        <img src="" alt="${_server.expired ? "expired" : "active"}">
+        <img src="https://crafatar.com/avatars/${_server.ownerUUID}?size=40&overlay" alt="">
+        <span>${_server.properties.name || ""}</span>
+        <span>${_server.minigameName ? "Minigame: " + _server.minigameName : _server.properties.description || ""}</span>
+        <span>${_server.owner}</span>
+        <button onclick="${owner ? "overview.selectRealm(" + _server.id + ")" : "overview.leaveRealm(" + _server.id + ")"}">${owner ? "Edit" : "Leave"}</button>
+      </div>`;
         //TODO add status of realm
+    }
+    function sortRealms(a, b) {
+        let owner = getCookie("name");
+        if (a.owner == owner && b.owner != owner)
+            return -1;
+        if (a.owner != owner && b.owner == owner)
+            return 1;
+        if (a.expired && !b.expired)
+            return 1;
+        if (!a.expired && b.expired)
+            return -1;
+        return a.owner > b.owner;
     }
     function selectRealm(id) {
         setCookie("worldid", id.toString());
--- server-npm/java/realms.ts
+++ server-npm/java/realms.ts
@@ -21,16 +21,21 @@ namespace overview {
       xhr.send(JSON.stringify(data));
       if (xhr.response) {
         let result = JSON.parse(xhr.response);
-        console.log(result);
         if (result.error) {
           displayError(result.error);
           return;
         }
         if (result.servers && result.servers.length > 0) {
+          console.log(result.servers);
+          result.servers = result.servers.sort(sortRealms);
           realmsList.innerHTML = "";
+          console.log(result.servers);
           for (let s of result.servers) {
+            // console.log(s);
             createOneRealm(s, data.name);
           }
+        } else {
+          realmsList.innerHTML = "Nothing to see here. You don't seem to have";
         }
       }
     } catch (error) {
@@ -41,14 +46,24 @@ namespace overview {
   function createOneRealm(_server: Server, ownerName: string) {
     let owner: boolean = _server.owner == ownerName;
     realmsList.innerHTML +=
-      `<div class="realm">
-  <img src="https://crafatar.com/avatars/${_server.ownerUUID}?size=40&overlay" alt="">
-  <span>${_server.properties.name || ""}</span>
-  <span>${_server.minigameName ? "Minigame: " + _server.minigameName : _server.properties.description || ""}</span>
-  <span>${_server.owner}</span>
-  <button onclick="${owner ? "overview.selectRealm(" + _server.id + ")" : "overview.leaveRealm(" + _server.id + ")"}">${owner ? "Edit" : "Leave"}</button>
-  </div>`;
+      `<div class="realm ${owner ? "owned" : "notOwned"}">
+        <img src="" alt="${_server.expired ? "expired" : "active"}">
+        <img src="https://crafatar.com/avatars/${_server.ownerUUID}?size=40&overlay" alt="">
+        <span>${_server.properties.name || ""}</span>
+        <span>${_server.minigameName ? "Minigame: " + _server.minigameName : _server.properties.description || ""}</span>
+        <span>${_server.owner}</span>
+        <button onclick="${owner ? "overview.selectRealm(" + _server.id + ")" : "overview.leaveRealm(" + _server.id + ")"}">${owner ? "Edit" : "Leave"}</button>
+      </div>`;
     //TODO add status of realm
+  }
+
+  function sortRealms(a: Server, b: Server) {
+    let owner: string = getCookie("name");
+    if (a.owner == owner && b.owner != owner) return -1;
+    if (a.owner != owner && b.owner == owner) return 1;
+    if (a.expired && !b.expired) return 1;
+    if (!a.expired && b.expired) return -1;
+    return a.owner > b.owner;
   }
 
   export function selectRealm(id: number) {
