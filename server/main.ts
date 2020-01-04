9bec3d2a9d4fdf870580737b7ad1db37d63eb4fe
Lukas Scheuerle (Plagiatus) <lukas@spark-squared.com>
2020-01-04 15:20:18 +0200

    removing useless comments

--- server/main.ts
+++ server/main.ts
@@ -76,28 +76,3 @@ async function handleRequest(_request: Http.IncomingMessage, _response: Http.Out
     _response.end();
   }
 }
-
-
-// let r: Client = new Client("token:7fa6518d5bbc40c7b9370bded454c150:e75e2d263b724a93a3e7a2491f4c454f", "1.15", "Plagiatus");
-// let r: Client = new Client("token:9c1b3949871441999761e0c3a12ca35b:64870cb3e3e14857bec6b9f597dc8b21", "1.15", "Plagypus");
-
-// let id: number = 4156375;
-// console.log(r.worlds.getWorld(4156375).detailInformation().sortPlayers().players[0]);
-// console.log(r.invites.);
-// async function test(){
-
-//   let auth: Auth = new Auth();
-//   let p: Player = new Player("therealplagiatus@gmail.com", "", "");
-//   try{
-//     await auth.validate(p);
-//   } catch(e){
-//     await auth.authenticate(p, "Scheuerle20.01.1995");
-//   }
-//   console.log(p);
-
-//   await auth.validate(p);
-
-//   let c: Client = new Client(`token:${p.token}:${p.uuid}`, "1.15", p.name);
-//   console.log(c);
-// }
-// test();
