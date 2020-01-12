60e90b3dc848ae92297215becab80d02e9c78771
Plagiatus <lukas.scheuerle@web.de>
2020-01-12 13:22:55 +0200

    switch slots

--- client/java/worlds/worlds.js
+++ client/java/worlds/worlds.js
@@ -4,10 +4,13 @@ var worldsPage;
     let worldid;
     let server;
     let selectedSlot;
+    let switchButtons;
     function init() {
         worldid = checkWorldId();
         checkCredentials();
         getWorlds();
+        switchButtons = document.getElementsByClassName("switch-slot-btn");
+        // settingsButtons = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName("world-settings-btn");
     }
     function getWorlds() {
         let data = getCredentials();
@@ -17,8 +20,7 @@ var worldsPage;
         if (result.error)
             return;
         console.log(result);
-        result.slots = new Map(result.slots);
-        server = result;
+        server = fixSlots(result);
         let slots = result.slots;
         let worlds = document.getElementById("worlds");
         worlds.innerHTML = "";
@@ -27,8 +29,8 @@ var worldsPage;
       <div class="world-wrapper ${i == result.activeSlot ? "active" : ""}" id="world-${i}">
         <img src="${slots.get(i).templateImage ? "data:image/png;base64, " + slots.get(i).templateImage : "../img/placeholder.png"}" alt="" class="world-image">
         <span class="world-name">${slots.get(i).slotName || `World ${i}`}</span>
-        <button disabled>Switch</button>
-        <button onclick="worldsPage.showSettings(${i})" >World Settings</button>
+        <button class="switch-slot-btn" onclick="worldsPage.switchTo(${i})" ${i == result.activeSlot ? "disabled" : ""}>Switch</button>
+        <button class="world-settings-btn" onclick="worldsPage.showSettings(${i})" >World Settings</button>
         <button disabled>Replace World</button>
       </div>
       `;
@@ -44,7 +46,10 @@ var worldsPage;
     function showSettings(slot) {
         if (!server)
             return;
+        if (selectedSlot)
+            document.querySelector("#world-" + selectedSlot + " .world-settings-btn").disabled = false;
         selectedSlot = slot;
+        document.querySelector("#world-" + slot + " .world-settings-btn").disabled = true;
         let slotOptions = server.slots.get(slot);
         document.getElementById("world-settings-wrapper").classList.remove("hidden");
         for (let setting in slotOptions) {
@@ -79,11 +84,39 @@ var worldsPage;
                 data.newSettings[e[0]] = e[1];
             }
         }
+        data.newSettings = JSON.stringify(data.newSettings);
         console.log(data);
     }
     worldsPage.saveWorldSettings = saveWorldSettings;
     function cancelWorldSettings() {
         document.getElementById("world-settings-wrapper").classList.add("hidden");
+        document.querySelector("#world-" + selectedSlot + " .world-settings-btn").disabled = false;
     }
     worldsPage.cancelWorldSettings = cancelWorldSettings;
+    function switchTo(slot) {
+        if (!server)
+            return;
+        for (let btn of switchButtons) {
+            btn.disabled = true;
+            btn.innerText = "Switching...";
+        }
+        let data = getCredentials();
+        data["command"] = "changeSlot";
+        data["world"] = worldid;
+        data["slot"] = slot;
+        let result = sendPOSTRequest(data);
+        for (let btn of switchButtons) {
+            btn.disabled = false;
+            btn.innerText = "Switch";
+        }
+        if (result.error) {
+            document.querySelector("#world-" + server.activeSlot + " .switch-slot-btn").disabled = true;
+            return;
+        }
+        server = fixSlots(result);
+        document.querySelector("#world-" + server.activeSlot + " .switch-slot-btn").disabled = true;
+        document.querySelector(".active").classList.remove("active");
+        document.getElementById("world-" + slot).classList.add("active");
+    }
+    worldsPage.switchTo = switchTo;
 })(worldsPage || (worldsPage = {}));
--- client/java/worlds/worlds.ts
+++ client/java/worlds/worlds.ts
@@ -4,10 +4,13 @@ namespace worldsPage {
   let worldid: number;
   let server: RealmsServer;
   let selectedSlot: number;
+  let switchButtons: HTMLCollectionOf<HTMLButtonElement>;
   function init() {
     worldid = checkWorldId();
     checkCredentials();
     getWorlds();
+    switchButtons = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName("switch-slot-btn");
+    // settingsButtons = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName("world-settings-btn");
   }
 
   function getWorlds() {
@@ -17,8 +20,7 @@ namespace worldsPage {
     let result: RealmsServer = sendPOSTRequest(data);
     if (result.error) return;
     console.log(result);
-    result.slots = new Map(result.slots);
-    server = result;
+    server = fixSlots(result);
     let slots = result.slots;
     let worlds: HTMLDivElement = <HTMLDivElement>document.getElementById("worlds");
     worlds.innerHTML = "";
@@ -27,8 +29,8 @@ namespace worldsPage {
       <div class="world-wrapper ${i == result.activeSlot ? "active" : ""}" id="world-${i}">
         <img src="${slots.get(i).templateImage ? "data:image/png;base64, " + slots.get(i).templateImage : "../img/placeholder.png"}" alt="" class="world-image">
         <span class="world-name">${slots.get(i).slotName || `World ${i}`}</span>
-        <button disabled>Switch</button>
-        <button onclick="worldsPage.showSettings(${i})" >World Settings</button>
+        <button class="switch-slot-btn" onclick="worldsPage.switchTo(${i})" ${i == result.activeSlot ? "disabled" : ""}>Switch</button>
+        <button class="world-settings-btn" onclick="worldsPage.showSettings(${i})" >World Settings</button>
         <button disabled>Replace World</button>
       </div>
       `;
@@ -44,21 +46,23 @@ namespace worldsPage {
 
   export function showSettings(slot: number) {
     if (!server) return;
+    if(selectedSlot) (<HTMLButtonElement>document.querySelector("#world-" + selectedSlot + " .world-settings-btn")).disabled = false;
     selectedSlot = slot;
+    (<HTMLButtonElement>document.querySelector("#world-" + slot + " .world-settings-btn")).disabled = true;
     let slotOptions = server.slots.get(slot);
     document.getElementById("world-settings-wrapper").classList.remove("hidden");
-    for(let setting in slotOptions){
-      let input: HTMLInputElement = <HTMLInputElement> document.getElementById(setting);
-      if(!input) continue;
-      if(input.type == "checkbox"){
+    for (let setting in slotOptions) {
+      let input: HTMLInputElement = <HTMLInputElement>document.getElementById(setting);
+      if (!input) continue;
+      if (input.type == "checkbox") {
         input.checked = slotOptions[setting];
       } else {
         input.value = slotOptions[setting];
       }
     }
   }
-  
-  export function saveWorldSettings(){
+
+  export function saveWorldSettings() {
     let form: HTMLFormElement = <HTMLFormElement>document.getElementById("world-settings");
     let data: any = getCredentials();
     data["command"] = "worldSettings";
@@ -66,20 +70,44 @@ namespace worldsPage {
     data["slot"] = selectedSlot;
     data["newSettings"] = {};
     let formdata: FormData = new FormData(form);
-    for(let e of formdata.entries()){
-      if(e[1] == "false" || e[1] == "true"){
+    for (let e of formdata.entries()) {
+      if (e[1] == "false" || e[1] == "true") {
         data.newSettings[e[0]] = e[1] == "true";
       }
-      else if(!isNaN(Number(e[1]))){
+      else if (!isNaN(Number(e[1]))) {
         data.newSettings[e[0]] = Number(e[1]);
       } else {
         data.newSettings[e[0]] = e[1];
       }
     }
+    data.newSettings = JSON.stringify(data.newSettings);
     console.log(data);
   }
 
-  export function cancelWorldSettings(){
+  export function cancelWorldSettings() {
     document.getElementById("world-settings-wrapper").classList.add("hidden");
+    (<HTMLButtonElement>document.querySelector("#world-" + selectedSlot + " .world-settings-btn")).disabled = false;
+  }
+
+  export function switchTo(slot: number) {
+    if (!server) return;
+    for (let btn of switchButtons) { btn.disabled = true; btn.innerText = "Switching..."; }
+    let data = getCredentials();
+    data["command"] = "changeSlot";
+    data["world"] = worldid;
+    data["slot"] = slot;
+    let result = sendPOSTRequest(data);
+    for (let btn of switchButtons) {
+      btn.disabled = false;
+      btn.innerText = "Switch";
+    }
+    if (result.error) {
+      (<HTMLButtonElement>document.querySelector("#world-" + server.activeSlot + " .switch-slot-btn")).disabled = true;
+      return;
+    }
+    server = fixSlots(result);
+    (<HTMLButtonElement>document.querySelector("#world-" + server.activeSlot + " .switch-slot-btn")).disabled = true;
+    document.querySelector(".active").classList.remove("active");
+    document.getElementById("world-" + slot).classList.add("active");
   }
 }
